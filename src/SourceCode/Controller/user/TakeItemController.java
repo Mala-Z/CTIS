e2c6e69772665bfa38128dc7b96c0b42f5cf68a2
Ppa13 <patrunjel.paula@gmail.com>
2016-04-24 19:20:51 +0200

    tableView in TakeItem solved

--- src/SourceCode/Controller/user/TakeItemController.java
+++ src/SourceCode/Controller/user/TakeItemController.java
@@ -1,17 +1,27 @@
 package SourceCode.Controller.user;
 
 import SourceCode.BusinessLogic.BusinessLogic;
+import SourceCode.BusinessLogic.Factory;
 import SourceCode.Controller.RunView;
+import SourceCode.Model.BorrowedItem;
+import SourceCode.Model.Employee;
+import SourceCode.Model.Item;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Timestamp;
 
 public class TakeItemController {
     BusinessLogic businessLogic = new BusinessLogic();
+    Factory factory = Factory.getInstance();
 
-    ObservableList<String> placeList = FXCollections.observableArrayList("Address", "Car", "One day use");
+    ObservableList<String> placeList = FXCollections.observableArrayList("Address", "Car");
+    private ObservableList takeItemData = FXCollections.observableArrayList();
 
     @FXML
     private TextField tfEmployeeBarcode;
@@ -27,12 +37,18 @@ public class TakeItemController {
     private Button btnSubmit;
     @FXML
     private ComboBox placeCombo;
+    @FXML
+    private TableColumn itemNumberColumn;
+    @FXML
+    private TableColumn itemNameColumn;
+    @FXML
+    private TableColumn placeColumn;
 
     private RunView runView;
 
-   @FXML
-    private void initialize(){
-       placeCombo.setItems(placeList);
+    @FXML
+    private void initialize() {
+        placeCombo.setItems(placeList);
     }
 
 
@@ -40,41 +56,35 @@ public class TakeItemController {
     private void btnSubmit() {
         try {
             if (tfEmployeeBarcode.getLength() > 0 && tfItemBarcode.getLength() > 0) {
-                if (!businessLogic.searchItem(Integer.parseInt(tfItemBarcode.getText()))) {
-                    java.util.Date today = new java.util.Date();
-                    Timestamp timeTaken = new Timestamp(today.getTime());
-                    businessLogic.takeItem(Integer.parseInt(tfEmployeeBarcode.getText()), Integer.parseInt(tfItemBarcode.getText()), timeTaken, placeCombo.getValue().toString());
-                    updateAlertMessage("Registration successful");
-                    runView.showMainView();
-                } else if (businessLogic.searchItem(Integer.parseInt(tfItemBarcode.getText()))) {
-                    updateAlertMessage("Item has been already taken by another employee");
-                    tfItemBarcode.setText(null);
-                }else if (placeCombo.getSelectionModel().getSelectedIndex() > 0) {
-                        updateAlertMessage("Missing place for item");
-                        tfItemBarcode.setText(null);
-                }
+                java.util.Date today = new java.util.Date();
+                Timestamp timeTaken = new Timestamp(today.getTime());
+                businessLogic.takeItem(Integer.parseInt(tfEmployeeBarcode.getText()), Integer.parseInt(tfItemBarcode.getText()), timeTaken, placeCombo.getValue().toString());
+                updateAlertMessage("Registration successful");
+                runView.showMainView();
+
             } else {
                 updateAlertMessage("Missing barcode for employee or item");
             }
+
         } catch (Exception e) {
             System.out.println("Exception in btnSubmit() from TakeItemController class:" + e.getMessage());
         }
     }
 
     @FXML
-    private void btnBack(){
-        try{
-
-        }catch (Exception e){
+    private void btnBack() {
+        try {
+            runView.showMainView();
+        } catch (Exception e) {
             System.out.println("Exception in btnBack() from TakeItemController class: " + e.getMessage());
         }
     }
 
     @FXML
-    private void btnDelete(){
-        try{
+    private void btnDelete() {
+        try {
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Exception in btnDelete() from TakeItemController class: " + e.getMessage());
         }
     }
@@ -97,15 +107,54 @@ public class TakeItemController {
     private void checkItemBarcode() {
         try {
             if (businessLogic.checkItemBarcode(Integer.parseInt(tfItemBarcode.getText()))) {
-                tableView.requestFocus();
-                //show scanned item in the list
-            } else {
+                if (!businessLogic.searchItem(Integer.parseInt(tfItemBarcode.getText()))) {
+                    populateTableView();
+                    tableView.requestFocus();
+                } else if (businessLogic.searchItem(Integer.parseInt(tfItemBarcode.getText()))) {
+                    updateAlertMessage("Item has been already taken by another employee");
+                    tfItemBarcode.setText(null);
+                }
+            } else if (!businessLogic.checkItemBarcode(Integer.parseInt(tfItemBarcode.getText()))) {
                 updateAlertMessage("Please scan the barcode again");
                 tfItemBarcode.setText(null);
             }
         } catch (Exception e) {
             System.out.println("Exception in checkItemBarcode() from TakeItemController class: " + e.getMessage());
         }
+    }
+
+    /* METHOD FOR POPULATING THE TABLE VIEW */
+    public void populateTableView() {
+
+        try {
+            /* SQL QUERY */
+            String sql = "SELECT itemNo, itemName FROM Item WHERE itemBarcode = ?;";
+
+            /* EXECUTION OF QUERY */
+            int inputBarcode = Integer.parseInt(tfItemBarcode.getText());
+            PreparedStatement preparedStatement = factory.preparedStatement(sql);
+            preparedStatement.setInt(1, inputBarcode);
+
+            ResultSet result = preparedStatement.executeQuery();
+
+            while ((result.next())) {
+                Item item = new Item();
+                item.itemNoProperty().set(result.getString("itemNo"));
+                item.itemNameProperty().set(result.getString("itemName"));
+                takeItemData.add(item);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Exception in populateTableView() from TakeItemController class: " + e.getMessage());
+        }
+
+        /* SETTING UP COLUMNS FOR TABLE VIEW */
+        itemNumberColumn.setCellValueFactory(new PropertyValueFactory<Employee, String>("itemNo"));
+        itemNameColumn.setCellValueFactory(new PropertyValueFactory<Item, String>("itemName"));
+        placeColumn.setCellValueFactory(new PropertyValueFactory<BorrowedItem, String>("place"));
+
+        /* ADDING THE OBSERVABLE LIST TO THE TABLE VIEW */
+        tableView.getItems().addAll(takeItemData);
     }
 
     /* METHOD FOR THE ALERT MESSAGES SHOWN TO THE USER */
